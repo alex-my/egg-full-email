8713d29cb0306514c5a4c52d7eaa784ffd17acb0
alex <alex_my@126.com>
2020-03-23 10:58:06 +0800

    提升依赖

--- test/full-email.test.js
+++ test/full-email.test.js
@@ -8,7 +8,7 @@ const {
 describe('test/full-email.test.js', () => {
   it('should success resolve', async () => {
     // use egg-full-email config
-    const to = 'alex_my@126.com';
+    const to = 'dst email address';
     const subject = 'hi egg-full-email';
     const html = '<p><span style="color: red">hello world</span></p>';
     const result = await app.fullEmail.sender(to, subject, html);
